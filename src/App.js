cfdea1492f404fcf79bf6bf988c78e5d0619a3cc
kangyoungju <eodwn2@naver.com>
2020-01-14 23:52:46 +0900

    react-todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
 
 function App() {
+  const name = "리액트";
+  const style = {
+    backgroundColor : 'gray',
+    clolr : 'black',
+    fontSize : '17px'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      {name === '리액트' && <h1>리액트입니다.</h1>}
+
+      <div style={{
+        color : 'red',
+        fontSize : '24px'
+      }}>{name}</div>
+
     </div>
   );
 }
