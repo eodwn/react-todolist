0d0935141cc267942e749aac9c6f6b9fcbc234b9
kangyoungju <eodwn2@naver.com>
2020-01-15 23:53:38 +0900

    컴포넌트 props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, {Fragment} from 'react';
+import React, {Component} from 'react';
+import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = "리액트";
-  const style = {
-    backgroundColor : 'gray',
-    clolr : 'black',
-    fontSize : '17px'
-  }
-
+const App = () => {
   return (
-    <div style={style}>
-      {name === '리액트' && <h1>리액트입니다.</h1>}
-
-      <div style={{
-        color : 'red',
-        fontSize : '24px'
-      }}>{name}</div>
-
-    </div>
-  );
-}
+    <MyComponent name="React" favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  )
+};
 
 export default App;
